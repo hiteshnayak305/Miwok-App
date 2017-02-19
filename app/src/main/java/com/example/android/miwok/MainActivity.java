74290a190bd152e765148aa5a878cc3c43a9ad05
Hitesh Nayak <hiteshnayak305@gmail.com>
2017-02-19 23:33:03 +0530

    using Event lstener OnClickListener instead of Intents

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,45 +13,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /**
-     * create onClick behaviour for numbers
-     *
-     * @param view
-     */
-    public void numbers(View view) {
-        Intent intent = new Intent(MainActivity.this, NumberActivity.class);
-        startActivity(intent);
-    }
+        /**
+         * create own Listener class from abstract listener View.OnClickListener class
+         * and override onClick method to deploy action
+         *
+         * OR
+         * we can completely replace numbersL-> with new OnClickListener() {
+         *                                                  @Override
+         *                                                   public void onClick(View v) {
+         *                                                      Intent intent = new Intent(MainActivity.this,NumberActivity.class);
+         *                                                      startActivity(intent);
+         *                                                      }
+         *                                                  }
+         */
+        class NumbersClickListener implements OnClickListener {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NumberActivity.class);
+                startActivity(intent);
+            }
+        }
+        NumbersClickListener numbersL = new NumbersClickListener();   // create instance of modified class listener
+        TextView numbersView = (TextView) findViewById(R.id.numbers);  // create object of target TextView
+        numbersView.setOnClickListener(numbersL);                     // set click listener to numbersView object
 
-    /**
-     * create onClick behaviour for family
-     *
-     * @param view
-     */
-    public void family(View view) {
-        Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
-        startActivity(intent);
-    }
+        //family
+        TextView familyView = (TextView) findViewById(R.id.family);  // create object of target TextView
+        familyView.setOnClickListener(new OnClickListener() {       // set click listener to numbersView object
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(intent);
+            }
+        });
 
-    /**
-     * create onClick behaviour for colors
-     *
-     * @param view
-     */
-    public void colors(View view) {
-        Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
-        startActivity(intent);
-    }
-
-    /**
-     * create onClick behaviour for phrases
-     *
-     * @param view
-     */
-    public void phrases(View view) {
-        Intent intent = new Intent(MainActivity.this, PhrasesActivity.class);
-        startActivity(intent);
+        //colors
+        TextView colorsView = (TextView) findViewById(R.id.colors);  // create object of target TextView
+        colorsView.setOnClickListener(new OnClickListener() {       // set click listener to numbersView object
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(intent);
+            }
+        });
+        //phrases
+        TextView phrasesView = (TextView) findViewById(R.id.phrases);  // create object of target TextView
+        phrasesView.setOnClickListener(new OnClickListener() {         // set click listener to numbersView object
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
