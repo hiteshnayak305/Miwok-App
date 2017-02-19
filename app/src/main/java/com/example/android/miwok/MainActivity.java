db2379e0691435c1059d6d8ab884798eaeeb614a
Hitesh Nayak <hiteshnayak305@gmail.com>
2017-02-19 23:22:17 +0530

    using Intents for navigation between activities

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * create onClick behaviour for numbers
+     *
+     * @param view
+     */
+    public void numbers(View view) {
+        Intent intent = new Intent(MainActivity.this, NumberActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * create onClick behaviour for family
+     *
+     * @param view
+     */
+    public void family(View view) {
+        Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * create onClick behaviour for colors
+     *
+     * @param view
+     */
+    public void colors(View view) {
+        Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * create onClick behaviour for phrases
+     *
+     * @param view
+     */
+    public void phrases(View view) {
+        Intent intent = new Intent(MainActivity.this, PhrasesActivity.class);
+        startActivity(intent);
+    }
 }
