76900e435785d5626e8e38ca176d3070fc9482b8
Hitesh Nayak <hiteshnayak305@gmail.com>
2017-02-20 22:27:33 +0530

    create list of words and add them as text view(NumbersActivity)

--- app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -1,13 +1,39 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static com.example.android.miwok.R.layout.activity_number;
 
 public class NumberActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_number);
+        setContentView(activity_number);
+
+        //creating List of words
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+        //adding views to activity
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.activity_number);
+        for (int i = 0; i < words.size(); i++) {
+            TextView word = new TextView(this);
+            word.setText(words.get(i));
+            rootView.addView(word);
+        }
     }
 }
